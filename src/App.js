b216eeecb5db4285c7e7b25a6223db24d92648f6
Ken <kenmarut@gmail.com>
2019-06-29 08:54:27 -0500

    work on splash screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navigation from "./components/Navigation";
+import Splash from "./components/Splash";
+import About from "./components/About";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./styles/App.scss";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
-      <Route exact path="/" />
+      <Route exact path="/" render={() => <Splash />} />
+      <Route exact path="/about" render={() => <About />} />
     </BrowserRouter>
   );
 }
