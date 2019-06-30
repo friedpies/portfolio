d78f25fd2007c2cbaa8b33b0e0a13c7eff07d0a5
Ken <kenmarut@gmail.com>
2019-06-30 18:07:52 -0500

    quick image resizing

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,20 @@ const projects = [
       "https://kjmporfolio.nyc3.digitaloceanspaces.com/projects/glophones/thumbnail.gif",
     gallery: [],
     video: "null"
+  },
+  {
+    name: "Bredboard",
+    thumbnail:
+      "https://kjmporfolio.nyc3.digitaloceanspaces.com/projects/bredboard/thumbnail.gif",
+    gallery: [],
+    video: "null"
+  },
+  {
+    name: "La Java",
+    thumbnail:
+      "https://kjmporfolio.nyc3.digitaloceanspaces.com/projects/la_java/thumbnail.png",
+    gallery: [],
+    video: "null"
   }
 ];
 
